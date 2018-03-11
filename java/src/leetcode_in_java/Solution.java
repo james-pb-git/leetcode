ec0b66cdc29ca003b36c89909abec2672cc1ac49
bo.pang <bo.pang@dianping.com>
2018-03-11 21:44:59 +0800

    Remove expired codes.

--- java/src/leetcode_in_java/Solution.java
+++ java/src/leetcode_in_java/Solution.java
@@ -11,36 +11,8 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class Solution {
 
-    public int findDuplicate(int[] nums) {
-
-        if( null == nums || 0 == nums.length )
-            return 0;
-        if( 1 == nums.length )
-            return nums[ 0 ];swap
-
-        int len = nums.length;
-        int N = len - 1;
-
-        int left = 0;
-        int right = N;
-        int mid, cntMid;
-        while( left <= right ) {
-            mid = left + ( right - left ) / 2;
-            cntMid = 0;
-            for( int idx = 0; idx < len; idx ++ ) {
-                if (nums[idx] < mid)
-                    cntMid++;
-            }
-            if( cntMid < len / 2 )
-                left = mid + 1;
-            if( cntMid > len / 2 )
-                right = mid - 1;
-        }
-
-    }
-
    public static void main( String args[] ) {
        System.out.println("Hello Leetcode");
-       Solution solution = new Solution();
+       Solution solution = new, Solution();
     }
 }
