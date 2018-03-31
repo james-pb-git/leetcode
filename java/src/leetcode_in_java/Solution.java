005271f6737c6e89961c70206b6cf0ff773a68a0
James <jamesjiongsi@gmail.com>
2018-03-31 22:49:14 +0800

    Letter combinations. Accepted but not efficient enough.

--- java/src/leetcode_in_java/Solution.java
+++ java/src/leetcode_in_java/Solution.java
@@ -14,10 +14,6 @@ public class Solution {
     public static void main( String args[] ) {
         System.out.println("Hello Leetcode");
         Solution solution = new Solution();
-        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> res = solution.threeSum(nums);
-        for (List<Integer> ans: res) {
-            System.out.println(ans);
-        }
+        //System.out.println(solution.letterCombinations("23"));
     }
 }
