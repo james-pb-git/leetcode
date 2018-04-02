91fd164189321bfc32e18045f1c416dda857dced
James <jamesjiongsi@gmail.com>
2018-04-02 10:00:18 +0800

    Solved merge k lists.

--- java/src/leetcode_in_java/Solution.java
+++ java/src/leetcode_in_java/Solution.java
@@ -14,6 +14,5 @@ public class Solution {
     public static void main( String args[] ) {
         System.out.println("Hello Leetcode");
         Solution solution = new Solution();
-        //System.out.println(solution.letterCombinations("23"));
     }
 }
