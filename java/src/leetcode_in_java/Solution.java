e72b17076a969435961e7840b8809d3a1c4dd583
James <jamesjiongsi@gmail.com>
2018-08-03 18:23:33 +0800

    Sovled Banada problem (binary search & ceil technique.) Improve search insert position solution (I think it is clearer and cleaner.)

--- java/src/leetcode_in_java/Solution.java
+++ java/src/leetcode_in_java/Solution.java
@@ -15,34 +15,11 @@ public class Solution {
         return 0;
     }
 
-    private List<Integer> accWeight = new ArrayList<Integer>();
-    public Solution(int[] w) {
-        int curAcc = 0;
-        for (int ele : w) {
-            curAcc += ele;
-            accWeight.add(curAcc);
-        }
-    }
 
-    public int pickIndex() {
-        double randomVal = Math.random() * this.accWeight.get(accWeight.size() - 1);
-        int left = 0, right = this.accWeight.size() - 1, mid;
-        while (left < right) {
-            mid = left + (right - left)/2;
-            if (randomVal <= accWeight.get(mid))
-                right = mid;
-            else
-                left = mid + 1;
-        }
-        return left;
-    }
 
     public static void main( String args[] ) {
         System.out.println("Hello Leetcode");
-        int[] w = new int[]{1,3};
-        Solution solution = new Solution(w);
-        for (int i = 0; i < 50; i++)
-            System.out.println(solution.pickIndex());
+        Solution solution = new Solution();
 
         long startTime = System.nanoTime();
         long endTime = System.nanoTime();
