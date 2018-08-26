76d75624f42475877152f00ea3ded5ceb0831a0c
James <jamesjiongsi@gmail.com>
2018-08-26 11:26:19 +0800

    Contest 99.

--- java/src/leetcode_in_java/Solution.java
+++ java/src/leetcode_in_java/Solution.java
@@ -12,6 +12,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class Solution {
 
     public static void main( String args[] ) {
+
         System.out.println("Hello Leetcode");
         Solution solution = new Solution();
 
